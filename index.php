56b3879e3d877c0826d333e2eedd6e0ab8095ed0
locvankhoiC1020G1 <locvankhoi@gmail.com>
2020-12-07 10:41:02 +0700

    thoi gian sv

---
+++ index.php
@@ -0,0 +1,10 @@
+//echo "Từ giờ mỗi project thì sẽ mở bằng cách này: localhost/<span>tên project</span>";
+//localhost/demo
+<html>
+<head>
+    <title>Thời gian hiện tại</title>
+</head>
+<body>
+<h1> Bây giờ là: <?php echo date('Y-M-d h:m:s'); ?> </h1>
+</body>
+</html>
